0574a1615ed55236b00c2ece6434ef351cb75ca5
Marccus Zavadzki <marccus.zavadzki@ambevtech.com.br>
2023-09-18 18:44:06 -0300

    add docker file

--- FileDivider/FileDivider.Api/Program.cs
+++ FileDivider/FileDivider.Api/Program.cs
@@ -19,11 +19,11 @@ builder.Services.AddCors(x => {
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if(app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if(app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseCors("AllowAll");
 
